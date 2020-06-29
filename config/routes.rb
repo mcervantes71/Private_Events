36589a2ad0e876d716e328c0c32d8c1be926d05e
Martin Cervantes <cervantes.martine@gmail.com>
2020-06-28 18:44:51 -0600

    Add devise view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "users#index"
+  root to: "events#index"
+
+  resources :users, only: [:index, :show]
+
+  resources :events do
+    resources :attendees, only: [:create, :destroy]
+  end
 end
