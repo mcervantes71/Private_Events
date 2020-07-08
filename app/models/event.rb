ebd4041bb4a3d7eb44efc7244043b75352467487
Martin Cervantes <cervantes.martine@gmail.com>
2020-07-07 19:09:12 -0600

    Fix stickler errors

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :attendees, foreign_key: :attended_event_id, dependent: :destroy
-	has_many :attendees, through: :attendees, source: :attendee
+  has_many :attendees, through: :attendees, source: :attendee
 
   validates :name, length: { in: 4..30 }, presence: true, uniqueness: true
   validates :details, length: { in: 4..50 }, presence: true
